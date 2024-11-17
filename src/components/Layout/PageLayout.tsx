7af7b312c80865f84e021151b02a89060be12cc7
Nick <nick@Nicks-MacBook-Pro-2.local>
2024-11-17 17:14:59 +0000

    feat: implemented grid layout with widgets for PageLayout.tsx

--- src/components/Layout/PageLayout.tsx
+++ src/components/Layout/PageLayout.tsx
@@ -1,12 +1,25 @@
 import { Navbar } from '@/components/Navbar/Navbar.tsx';
 import { Outlet } from 'react-router-dom';
 import { BottomBar } from '@/components/Navbar/BottomBar.tsx';
+import { Grid2 as Grid } from '@mui/material';
 
 export const PageLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Grid container spacing={2}>
+        <Grid
+          size={{ sm: 3, md: 2 }}
+          sx={{ display: { xs: 'none', md: 'block' } }}>
+          Left Widgets
+        </Grid>
+        <Grid size={{ sm: 9, md: 8 }}>
+          <Outlet />
+        </Grid>
+        <Grid size={{ md: 2 }} sx={{ display: { xs: 'none', md: 'block' } }}>
+          Right Widgets
+        </Grid>
+      </Grid>
       <BottomBar />
     </>
   );
